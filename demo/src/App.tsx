9de33382afa8f8ec96f7904fbbbe1eb7a4ddc1a1
Hasmukh Suthar <hasmukhsuthar98@gmail.com>
2020-05-31 15:02:44 +0530

    add github repo link in demo site

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -2,14 +2,19 @@ import React from 'react';
 import './App.css';
 import { CompoundGraphPage } from './compound-graph-page';
 import logo from './logo.svg';
+import githubLogo from './github-logo.svg';
 
 function App() {
   return (
     <div className="main">
       <header className="header">
-        <a className="app-name" href="/">
+        <a className="app-name remove-decoration" href="/">
           <img alt="Logo" src={logo} />
           Nested Graph Generator
+        </a>
+        <a className="github-repo-link remove-decoration" href="https://github.com/zombierobo/nested-graph-generator" target="__blank">
+          <img alt="Github Logo" src={githubLogo} />
+          Github
         </a>
       </header>
       <div className="content">
