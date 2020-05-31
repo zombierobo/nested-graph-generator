40db0bc8c61221977e025fbe9ad53521fba6d1b5
Hasmukh Suthar <hasmukhsuthar98@gmail.com>
2020-05-31 13:55:01 +0530

    demo site

---
+++ precache-manifest.b270f604bbfe1025073c767529dd070d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62d6acb92e53a725617ec41a9676cda1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a9041174bd23b2c97b67",
+    "url": "/static/css/main.41d4da2d.chunk.css"
+  },
+  {
+    "revision": "bad22f0980e82aba7c63",
+    "url": "/static/js/2.21ccd1d6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.21ccd1d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9041174bd23b2c97b67",
+    "url": "/static/js/main.2124ec96.chunk.js"
+  },
+  {
+    "revision": "95641a527b764497fd6d",
+    "url": "/static/js/runtime-main.1d3647f4.js"
+  },
+  {
+    "revision": "e385133d1263445e1c6136f92a2b359d",
+    "url": "/static/media/logo.e385133d.svg"
+  }
+]);
