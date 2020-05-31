cf833399c171b88caed3c6edfd1820b806787311
Hasmukh Suthar <hasmukhsuthar98@gmail.com>
2020-05-31 14:21:46 +0530

    fix homepage path to github pages path

---
+++ precache-manifest.a0f94cad5a0db95adc646524c3d9e88a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a45c30581b41354382927571818c8c5",
+    "url": "/nested-graph-generator/index.html"
+  },
+  {
+    "revision": "a9041174bd23b2c97b67",
+    "url": "/nested-graph-generator/static/css/main.41d4da2d.chunk.css"
+  },
+  {
+    "revision": "bad22f0980e82aba7c63",
+    "url": "/nested-graph-generator/static/js/2.21ccd1d6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/nested-graph-generator/static/js/2.21ccd1d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9041174bd23b2c97b67",
+    "url": "/nested-graph-generator/static/js/main.2124ec96.chunk.js"
+  },
+  {
+    "revision": "f4cd294bb2dd7941f493",
+    "url": "/nested-graph-generator/static/js/runtime-main.835347f2.js"
+  },
+  {
+    "revision": "e385133d1263445e1c6136f92a2b359d",
+    "url": "/nested-graph-generator/static/media/logo.e385133d.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b270f604bbfe1025073c767529dd070d.js"
+  "/nested-graph-generator/precache-manifest.a0f94cad5a0db95adc646524c3d9e88a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/nested-graph-generator/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
